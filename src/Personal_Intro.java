c848bdaa6d61a317a19145b5bd2a3b26ed543041
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-14 11:37:51 -0600

    personal learning

---
+++ src/Personal_Intro.java
@@ -0,0 +1,65 @@
+public class Personal_Intro {
+
+        public static void main(String[] args){
+
+            // before we declare a variable, we think of the data type we want to assign
+            // to the variable is it ;
+            // an integer abbreviated int
+            // is a String. String uses " ", with the value inside the quotes
+
+            // int x; // declaration of a variable
+            // x = 123 // is called assignment
+            //int x = 123; // this is initialization
+            int x = 123;
+            System.out.println(x);
+
+            // long data type
+            long K = 1234567890123456789L;
+
+            // byte
+
+            byte g = 12;
+            System.out.println("My number is: " +g);
+
+            //float
+            float y = 3.13f;
+            System.out.println(y);
+
+            // double
+
+            double z = 3.1234;
+            System.out.println(z);
+
+            //boolean
+            boolean p = true;
+            System.out.println(p);
+
+            //char
+            char symbol = '@';
+            System.out.println(symbol);
+
+            //String
+            //note that since String is a reference data type, it starts with a capital letter
+            // All reference data types in Java starts with a capital letter
+            String name = "Mbenge";
+            System.out.println(name);
+            System.out.println("Welcome to " +name);
+
+            System.out.println("\\\t\"good to start\"");
+            System.out.println("good to start\n");
+
+
+            //switching two variables
+            String t = "water";
+            String h = "Kool-Aid";
+            String temp; // temp is a temporarily variable
+            temp = t;
+            t = h;
+            h = temp;
+
+            System.out.println("t: " +t);
+        }
+
+    }
+
+
---
+++ src/UsingScanner.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class UsingScanner {
+
+        public static void main(String[] args){
+
+            //Scanner helps us to get input from a user
+            //You must import the scanner before you are able to use it in java
+            // you can use a scanner to read the contents of a file
+            Scanner scanner = new Scanner (System.in);
+            System.out.println("What is your name");
+            String name = scanner.nextLine();
+            System.out.println("What's your age?");
+            int age = scanner.nextInt();
+            scanner.nextLine();// running the code without this scanner.nextLine(); here
+            // means that our scanner will not read the System.out.println
+            System.out.println("What's your best meal?");
+            String food = scanner.nextLine();
+
+
+            System.out.println("Hello " +name);
+            System.out.println("You are " +age+" years old");
+            System.out.println("You like " +food);
+
+        }
+    }
+
+
