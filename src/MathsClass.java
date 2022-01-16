9e9b4e184455d0838817b677130faf6d6ee10aab
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-15 21:41:35 -0600

    maths class

--- src/MathsClass.java
+++ src/MathsClass.java
@@ -1,5 +1,15 @@
 public class MathsClass {
     public  static  void main(String[] args){
 
+
+        double x = 3.14;
+        double y = -10;
+        //Max Method, used in finding the highest value between integers
+        //Min Method, finds the least of two values
+
+       double z =  Math.max(x,y);
+       double k = Math.min(x,y);
+        System.out.println("maximum value is "+z);
+        System.out.println("minimum value is "+k);
     }
 }
