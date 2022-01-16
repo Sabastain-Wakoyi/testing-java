b242b4968a3999783f1f5d6d37811fc748c7de49
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-15 19:47:27 -0600

    GUI application

---
+++ src/Expressions.java
@@ -0,0 +1,25 @@
+public class Expressions {
+    public static void main(String[] args) {
+
+
+        // expressions = operands & operators
+        //operands = values, variables, numbers, quantity
+        //operators = +, -, *, /, %
+
+       // int friends = 10;
+        //friends = friends / 3;
+       // friends++;
+       // System.out.println(friends);
+
+        //if we want to see the reminder when dividing by an integer,
+        //then we should use the data type "double"
+
+        double friends = 10;
+
+        friends = (double) friends / 3;
+
+        System.out.println(friends);
+
+
+    }
+}
