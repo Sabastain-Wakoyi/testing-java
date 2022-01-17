bf220a4bacdfccc6328a2aaa5f08917e89becb2e
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-16 20:29:38 -0600

    if statements

--- src/Statements.java
+++ src/Statements.java
@@ -1,2 +1,9 @@
 public class Statements {
+
+    public static void main(String[] args) {
+        // if statement; this performs a block of code if it's condition evaluates to be true.
+
+        int age = 18;
+        if(){
+    }
 }
