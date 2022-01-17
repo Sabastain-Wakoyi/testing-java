f624d046e532547412580b4d0937c6fc25179c1b
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-16 20:48:40 -0600

    if and else if statements

--- src/Statements.java
+++ src/Statements.java
@@ -3,12 +3,18 @@ public class Statements {
     public static void main(String[] args) {
         // if statement; this performs a block of code if it's condition evaluates to be true.
 
-        int age = 9;
+        int age = 5;
         if (age >=60) {
             System.out.println("Ok Boomer");
         }
         else  if(age>=18){
             System.out.println("You are an adult");
+        }
+        else if(age>=13){
+            System.out.println("You are a teenager");
+        }
+        else if(age == 5){
+            System.out.println("You are a kid");
         }
         else {
             System.out.println("You are not an adult");
