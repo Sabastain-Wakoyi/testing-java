9c4ccd87e6e2bebfa20bca4ee4aaa3a19faaca80
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-16 20:42:25 -0600

    if statements

--- src/Statements.java
+++ src/Statements.java
@@ -3,7 +3,15 @@ public class Statements {
     public static void main(String[] args) {
         // if statement; this performs a block of code if it's condition evaluates to be true.
 
-        int age = 18;
-        if(){
+        int age = 9;
+        if (age >=60) {
+            System.out.println("Ok Boomer");
+        }
+        else  if(age>=18){
+            System.out.println("You are an adult");
+        }
+        else {
+            System.out.println("You are not an adult");
+        }
     }
 }
