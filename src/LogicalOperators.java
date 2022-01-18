858184eaca8c20a4a41b6052e964a0b17bddaa98
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-17 20:39:42 -0600

    logical operators

---
+++ src/LogicalOperators.java
@@ -0,0 +1,9 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        // logical operators are used to connect two or more expressions
+
+        // && = (AND) both conditions must be true
+        // || = (OR) either condition must be true
+        //! = (NOT) reverses boolean value of condition
+    }
+}
