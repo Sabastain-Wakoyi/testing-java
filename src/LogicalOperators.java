1c311b84d76f7410b32f502bec01b02bb8e81dd6
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-17 20:58:06 -0600

    logical operators

--- src/LogicalOperators.java
+++ src/LogicalOperators.java
@@ -1,9 +1,48 @@
+import java.util.Scanner;
+
 public class LogicalOperators {
     public static void main(String[] args) {
         // logical operators are used to connect two or more expressions
 
         // && = (AND) both conditions must be true
         // || = (OR) either condition must be true
-        //! = (NOT) reverses boolean value of condition
+        // ! = (NOT) reverses boolean value of condition
+
+
+        // &&, AND logical operator
+//        int temp = 25;
+//        if(temp >=35){
+//            System.out.println("It is hot outside");
+//        }
+//        else if (temp >=20 && temp <= 35){
+//            System.out.println("It is warm outside");
+//        }
+//        else{
+//            System.out.println("It is cold outside");
+//        }
+
+        // ||, OR logical operator
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("You are playing a game!, Press q or Q to quit");
+//        String response = scanner.next();
+//        if(response.equals("q") || response.equals("Q")){
+//            System.out.println("You Quit the game");
+//        }
+//        else {
+//            System.out.println("You are still playing the game");
+//        }
+
+        // !, NOT logical operator
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("You are playing a game!, Press q or Q to quit");
+        String response = scanner.next();
+        if(!response.equals("q") && !response.equals("Q")){
+            System.out.println("You are still playing the game");
+        }
+        else {
+            System.out.println("You quit the game");
+        }
+
     }
 }
