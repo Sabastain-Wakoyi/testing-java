ea009f9a2c9602354f104f17314d2f47f8011473
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-02-25 22:50:22 -0600

    nestedloop

---
+++ src/NestedLoop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NestedLoop {
+    public static void main(String[] args) {
+        //Nested loops = a loop inside of a loop
+        Scanner scanner = new Scanner(System.in);
+        int rows;
+        int columns;
+        String symbol = " ";
+
+        System.out.println("Enter # of rows: ");
+        rows = scanner.nextInt();
+        System.out.println("Enter # of columns: ");
+        columns = scanner.nextInt();
+        System.out.println("Enter symbol to use");
+    }
+}
