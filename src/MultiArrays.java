4f359c7316dc4c956ce54eb8b337aba5cdc22333
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-04-13 10:16:25 -0500

    defined the array

--- src/MultiArrays.java
+++ src/MultiArrays.java
@@ -2,5 +2,11 @@ public class MultiArrays {
 
     public static void main (String[] args) {
         //2D array = an array of arrays
+
+        String[] [] cars = {
+                {"Camaro", "Corvette", "Silverado"},
+                {"INV", "Range", "Toyota"},
+                {"Ferrari", "Lambo", "Tesla"},
+        };
     }
 }
