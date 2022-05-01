d052187df4bb3cbc93e53d5318fe9124e995a145
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-04-30 21:13:56 -0500

    string reference

--- src/String.java
+++ src/String.java
@@ -3,5 +3,8 @@ public class String {
     public static void main (String[] args) {
         //string is a reference data type that can store one or more characters
         // reference data types have access to useful methods
+        String name = "Jevi";
+        boolean result = name.equals("Jevi");
+        System.out.println(result);
     }
 }
