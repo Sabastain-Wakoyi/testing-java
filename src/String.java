af2e543273e873256449dfbbbac40d4f67c080fd
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-04-30 21:09:02 -0500

    string reference data types

---
+++ src/String.java
@@ -0,0 +1,7 @@
+public class String {
+
+    public static void main (String[] args) {
+        //string is a reference data type that can store one or more characters
+        // reference data types have access to useful methods
+    }
+}
