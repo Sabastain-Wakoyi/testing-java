52348152ee7a409b78547ad3036d5dbc7dd95de7
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-07-01 16:40:25 -0500

    wrapper classes

--- src/Wrapper.java
+++ src/Wrapper.java
@@ -9,8 +9,17 @@ public class Wrapper {
         //.........   //........
         //boolean     Boolean
         //char        Character
-        // int        Integer
+        //int        Integer
         //double      Double
-        //
+
+        //autoboxing = the automatic conversion that the Java compiler makes between the primitive
+        // and their corresponding object classes
+        //unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primitive
+
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+
     }
 }
