98a28845968e7ceaf8fa8759571be7e14b314dbc
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-07-01 16:33:23 -0500

    wrapper

---
+++ src/Wrapper.java
@@ -0,0 +1,16 @@
+public class Wrapper {
+
+    public static void main (String[] args) {
+        //wrapper class = provides a way to use primitive data types as reference data types
+        // reference data types contain useful methods
+        // can be used with collections (ex. ArrayList)
+
+        //primitive    //wrapper
+        //.........   //........
+        //boolean     Boolean
+        //char        Character
+        // int        Integer
+        //double      Double
+        //
+    }
+}
