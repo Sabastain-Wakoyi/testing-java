c45ed25490d49e6c35429d52039afaaa91495df4
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-07-06 19:54:33 -0500

    ArrayList

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,2 +1,20 @@
 public class ArrayList {
+    //Arraylist = a resizable array
+    //Arraylist = Elements can be added and removed after compilation phase
+    //ArrayList, stores reference data types
+
+    public static void main(String[] args) {
+        ArrayList<String> mboua = new ArrayList<String>();
+
+        mboua.add("bieddwey");
+        mboua.add("chi");
+        mboua.add("veh");
+
+        //mboua.set(0, "mpweh");
+        mboua.remove(2);
+        mboua.clear();
+        for (int i = 0; i<mboua.size(); i++) {
+
+        }
+    }
 }
