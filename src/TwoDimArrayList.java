8250b2b696fb806e8d75918c88dfcfbb2260e345
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-07-11 23:54:34 -0500

    2d arraylist

--- src/TwoDimArrayList.java
+++ src/TwoDimArrayList.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class TwoDimArrayList {
     public static  void main (String[] args) {
+        // 2D ArrayList = a dynamic list of lists
+        // you can change the size of these lists during runtime
 
     }
 
