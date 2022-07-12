3b03d201937d325e58e66069cce5097f7e02a669
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-07-12 00:08:01 -0500

    2d arraylist

--- src/TwoDimArrayList.java
+++ src/TwoDimArrayList.java
@@ -5,6 +5,32 @@ public class TwoDimArrayList {
         // 2D ArrayList = a dynamic list of lists
         // you can change the size of these lists during runtime
 
+        ArrayList<ArrayList<String>> marketList = new ArrayList();
+
+        ArrayList<String> pamList = new ArrayList();
+        pamList.add("biscut");
+        pamList.add("garlic");
+        pamList.add("pizza");
+
+        ArrayList<String> cashList = new ArrayList();
+        cashList.add("tomatoes");
+        cashList.add("fertilizer");
+        cashList.add("maize seed");
+
+        ArrayList<String> drinksList = new ArrayList();
+        drinksList.add("top");
+        drinksList.add("malta");
+        drinksList.add("coca cola");
+        drinksList.add("UCB");
+
+        marketList.add(pamList);
+        marketList.add(cashList);
+        marketList.add(drinksList);
+
+        //System.out.println(pamList.get(0));
+        //System.out.println(pamList.get(0).get(0));
+        //System.out.println(pamList.get(2).get(1));
+        System.out.println(drinksList);
     }
 
 }
