1d084cfccc6b0c9f6a9b266f9e4cbe5a0b5f6950
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-07-13 19:42:14 -0500

    method

---
+++ src/Method.java
@@ -0,0 +1,9 @@
+public class Method {
+    public static void main(String[] args){
+        //method in java is a block of code that is executed whenever  it is called upon
+
+        int x = 7;
+        int y = 8;
+
+    }
+}
