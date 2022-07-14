bb6b309ba4a547890353a78204b3801a2eb9fd0e
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-07-13 19:39:08 -0500

    for each loop

--- src/ForEachLoop.java
+++ src/ForEachLoop.java
@@ -8,6 +8,11 @@ public class ForEachLoop {
 
        // String[] animals = {"dog", "dog", "rat", "bird"};
         ArrayList<String> animals = new ArrayList<String>();
+        animals.add("dog");
+        animals.add("cat");
+        animals.add("rat");
+        animals.add("bird");
+
 
         for (String i : animals) {
             System.out.println(i);
