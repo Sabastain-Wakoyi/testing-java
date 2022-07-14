b333e631673f9d9d21c85f4f3ba81766234ad917
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-07-13 19:35:08 -0500

    for each loop

---
+++ src/ForEachLoop.java
@@ -0,0 +1,13 @@
+public class ForEachLoop {
+
+    public static void main(String[] args){
+        //for-each loop also called enhanced for-loop = traversing technique to iterate through the elements
+        // in an array/collection
+        //for-each loop is less steps, more readable and disadvantageous that it's less flexible
+
+        String[] animals = {"dog", "dog", "rat", "bird"};
+        for (String i : animals) {
+            System.out.println(i);
+        }
+    }
+}
