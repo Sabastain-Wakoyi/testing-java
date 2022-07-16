c226d41e1cfbd31fc9ecb61e459618df6a8def19
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-07-16 12:59:08 -0500

    method overloading

---
+++ src/OverloadedMethods.java
@@ -0,0 +1,7 @@
+public class OverloadedMethods {
+    public static void main(String[] args){
+        //overloaded methods are methods that share the same name but have different parameters
+        // method name + parameters = method signature
+
+    }
+}
