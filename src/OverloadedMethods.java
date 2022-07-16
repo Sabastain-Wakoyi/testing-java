2da03c95ecd851483b7de9928a1c82e043459516
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-07-16 14:17:03 -0500

    method overloading

--- src/OverloadedMethods.java
+++ src/OverloadedMethods.java
@@ -2,8 +2,9 @@ public class OverloadedMethods {
     public static void main(String[] args) {
         //overloaded methods are methods that share the same name but have different parameters
         // method name + parameters = method signature
-        //double
-             int   x = add(1,2);
+
+            // int   x = add(1,2,3,4);
+             double   x = add(1.0,2.0,3.0,4.0);
         System.out.println(x);
     }
 
@@ -18,7 +19,22 @@ public class OverloadedMethods {
     }
 
     static int add(int a, int b, int c, int d) {
-        System.out.println("This is overloaded method #2");
+        System.out.println("This is overloaded method #3");
+        return a + b + c + d;
+    }
+
+    static double add(double a, double b) {
+        System.out.println("This is overloaded method #4");
+        return a + b;
+    }
+
+    static double add(double a, double b, double c) {
+        System.out.println("This is overloaded method #5");
+        return a + b + c;
+    }
+
+    static double add(double a, double b, double c, double d) {
+        System.out.println("This is overloaded method #6");
         return a + b + c + d;
     }
 
