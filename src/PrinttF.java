94ce0e9a8c769416c60e48616afd1a272695c5b6
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-07-19 22:57:57 -0500

    printf

---
+++ src/PrinttF.java
@@ -0,0 +1,10 @@
+public class PrinttF {
+
+    public static void main (String[] args){
+        //printf() is an optional method to control, format, and display text to the console window
+        // two arguments = format string + (object/variable/value)
+        //% [flags] [precision] [width] [conversion-character]
+
+        System.out.printf("This is a format string", 123);
+    }
+}
