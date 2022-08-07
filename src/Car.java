c2525f9864497635a5f2edf40d3c93dc61ea778e
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-08-07 18:51:07 -0500

    OOP

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,16 @@
 public class Car {
 
-    String make = "Toyota";
+    String make = "IMV";
     String model = "Camry";
     int year = 2020;
     String color = "red";
     double price = 50000.00;
+
+    void drive() {
+        System.out.println("You drive the car");
+    }
+    void brake (){
+        System.out.println("You step on the brakes");
+    }
+
 }
