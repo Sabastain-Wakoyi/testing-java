887a5f2bae23cacf32599c496be41a42e9a3bdad
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-08-07 18:31:23 -0500

    final

--- src/Ffinal.java
+++ src/Ffinal.java
@@ -1,10 +1,8 @@
 public class Ffinal {
     public static void main(String[] args){
+
         final double PI = 3.14159;
 
-        double pi = 3.14159;
-
-        pi = 4;
 
         System.out.println(PI);
     }
