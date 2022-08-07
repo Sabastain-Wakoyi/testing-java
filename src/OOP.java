5487a66e9020e42e8b236624532413f77cde2a07
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-08-07 18:54:24 -0500

    OOP

--- src/OOP.java
+++ src/OOP.java
@@ -3,5 +3,11 @@ public class OOP {
     public static void main(String[] args) {
         //object = an instance of a class that may contain attributes and methods
         // example :(desk, computer, wood)
+
+        Car myCar = new Car();
+
+        System.out.println(myCar.make);
+        System.out.println(myCar.model);
+
     }
 }
