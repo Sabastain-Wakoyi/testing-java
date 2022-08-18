f57e6560141046935615903e38d0378369755fba
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-08-18 10:50:12 -0500

    contructor

---
+++ src/Constructor.java
@@ -0,0 +1,6 @@
+public class Constructor {
+    public static void main(String[] args) {
+        // constructor is a special method that is called when an object is instantiated (created)
+
+    }
+}
