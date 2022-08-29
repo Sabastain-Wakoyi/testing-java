0c7b8fbc537b0e9dbe05e3236826a1490316fbac
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-08-29 11:47:14 -0500

    local and global variables

--- src/LocalGlobalVar.java
+++ src/LocalGlobalVar.java
@@ -1,2 +1,9 @@
 public class LocalGlobalVar {
+    public static void main (String[] args){
+        // local variables are declared inside a method
+        // and are visible only to that method
+
+        //global variables are declared outside a method, but within a class
+        //and are visible to all parts of a class
+    }
 }
