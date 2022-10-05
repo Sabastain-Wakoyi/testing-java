df806e72b7cf20247feb668f940713e39591a796
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-10-04 21:55:26 -0500

    use of static

--- src/Statik.java
+++ src/Statik.java
@@ -1,6 +1,9 @@
 public class Statik {
 
     public static void main(String[] args){
-        //
+        //static= modifier. A single copy of a variable/method is created and share.
+        // the class owns the static member
+
+
     }
 }
