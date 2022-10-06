46a1f2aad92dde05d06491e274c12a61b43f3450
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-10-05 22:37:17 -0500

    rest api

--- src/RESTAPI.java
+++ src/RESTAPI.java
@@ -6,6 +6,7 @@ public class RESTAPI {
         HttpRequest postRequest = HttpRequest.newBuilder()
                 .uri(new URI("https://api.assemblyai.com/v2/transcript"))
                 .header("Authorization", Constant.API_Key)
+                .POST(BodyPublisher.ofstring(""))
 
     }
 }
