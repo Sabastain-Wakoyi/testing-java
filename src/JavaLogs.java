efc7864093bd0de80818196a54b6bc1df2a04b5f
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-10-06 21:50:21 -0500

    java logs

---
+++ src/JavaLogs.java
@@ -0,0 +1,12 @@
+import java.util.logging.LogRecord;
+
+public class JavaLogs {
+    @Override
+    public void publish(LogRecord record) {
+        //add own logic to publish
+        super.publish(record);
+    }
+
+
+
+}
