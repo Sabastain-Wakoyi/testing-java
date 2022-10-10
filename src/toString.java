ded255cca3205e12ec64df4f147b922474592bfb
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-10-10 15:42:02 -0500

    tostring

--- src/toString.java
+++ src/toString.java
@@ -1,6 +1,10 @@
 public class toString {
 
     public  static void main(String[] args) {
+//toString(), is a special method that all objects inherit
+        //It also returns a string that textually represents an object
+        //can be used both implicitly and explicitly
+
 
 
     }
