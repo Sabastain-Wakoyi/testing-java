bef81b58391a64abb0643efbb65eade6c5f4ddc7
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-11-09 21:07:15 -0600

    login

--- src/Java_GUI.java
+++ src/Java_GUI.java
@@ -6,7 +6,10 @@ import java.lang.Exception;
 //class variables
 
 public class Java_GUI implements ActionListener{
-
+        private static JLabel password1, label;
+        private static JTextField username;
+        private static JButton button;
+        private static JPasswordField Password;
     @Override
     public void actionPerformed(ActionEvent e) {
 
